3d46f02bf658da01d4989385736366af8683eed5
khai <samincgs21@gmail.com>
2026-03-04 19:25:10 -0500

    added some guardrails to fix the app from breaking from bad inputs, now working on a ui

--- wordle.py
+++ wordle.py
@@ -1,10 +1,19 @@
 import os
+import sys
 import random
 
+import customtkinter as ctk
+
 PATH = 'data/'
+ALL_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+MAX_GUESS_LENGTH = 5
+MAX_GUESSES = 6
+
+CORRECT_ANSWER = 'g'
+SEMI_CORRECT_ANSWER = 'y'
+WRONG_ANSWER = 'x'
 
 def get_words_lists(path):
-    
     guessable_words_list = []
     answers_list = []
     
@@ -14,6 +23,7 @@ def get_words_lists(path):
             f = open(file_path)
             data = f.read().split('\n')
             f.close()
+            
             if 'guess' in file_path:
                 guessable_words_list.extend(data)
             if 'answer' in file_path:
@@ -22,17 +32,16 @@ def get_words_lists(path):
     return guessable_words_list, answers_list
         
 
-def pick_word(word_list): # add letter frequency
-    frequency = {w: 0 for w in 'abcdefghijklmnopqrstuvwxyz'}
+def pick_word(word_list):
+    frequency = {w: 0 for w in ALL_LETTERS}
+    best_word = None
+    best_score = -1
     
     for word in word_list:
         for letter in set(word):
             if letter in frequency:
                 frequency[letter] += 1
-                
-    best_word = None
-    best_score = -1
-    
+
     for word in word_list:
         unique_letters = set(word)
         score = 0
@@ -51,11 +60,11 @@ def get_feedback(word):
     
     for i in range(len(word)):
         if word[i] == wordle_word[i]:
-            feedback += 'g'
+            feedback += CORRECT_ANSWER
         elif word[i] != wordle_word[i] and word[i] in wordle_word:
-            feedback += 'y'
+            feedback += SEMI_CORRECT_ANSWER
         elif word[i] != wordle_word[i]:
-            feedback += 'x'
+            feedback += WRONG_ANSWER
     return feedback
 
 def apply_filter(word_list, current_word, feedback):
@@ -65,17 +74,17 @@ def apply_filter(word_list, current_word, feedback):
         keep = True
         
         for i in range(len(word)):
-            if feedback[i] == 'x':
+            if feedback[i] == WRONG_ANSWER:
                 if current_word[i] in word:
                     keep = False
                     break
                     
-            if feedback[i] == 'g':
+            if feedback[i] == CORRECT_ANSWER:
                 if current_word[i] != word[i]:
                     keep = False
                     break
                 
-            if feedback[i] == 'y':
+            if feedback[i] == SEMI_CORRECT_ANSWER:
                 if current_word[i] == word[i] or current_word[i] not in word:
                     keep = False
                     break
@@ -85,36 +94,44 @@ def apply_filter(word_list, current_word, feedback):
             
     return kept_words
     
+def validate_input(input_type, question):
+    allowed = set(CORRECT_ANSWER + WRONG_ANSWER + SEMI_CORRECT_ANSWER)
+    
+    while True:
+        response = input(str(question)).strip().lower()
+        if (len(response) != MAX_GUESS_LENGTH) or (not response.isalpha()):
+            print(f'That is not correct. Please enter another ' + input_type)
+            continue
+        if input_type == 'feedback':
+            if not set(response).issubset(allowed): 
+                print('Your feedback has incorrect inputs.')
+                continue
+        break
+    return response
 
-def play(ai=False, output=False):
-    answered_correct = False
-    initial_guess = True
-    filtered_list = answers_list
-    num_guess = 0
-        
-    while not answered_correct:      
+def play_in_terminal(ai):
+    global answered_correct, num_guess, initial_guess, filtered_list
+            
+    while (not answered_correct) and (num_guess < MAX_GUESSES):      
         if not ai:
-            guess = input('What is your guess: ').strip().lower()
+            guess = validate_input(input_type='guess', question='What is your guess? ')
         else:
-            if initial_guess:
-                guess = 'crane'
-            else:
-                guess = pick_word(filtered_list)
-        
+            guess = 'crane' if initial_guess else pick_word(filtered_list)
+            
         num_guess += 1
-                 
+        print(num_guess)
         if not ai:
-            feedback = input('What is the colors of the answer: ')
-        else: 
+            feedback = validate_input(input_type='feedback', question='What is your feedback? ')
+        else:
             feedback = get_feedback(guess)  
-        
+
         if initial_guess:
             filtered_list = apply_filter(answers_list, guess, feedback)
             initial_guess = False
         else: 
             filtered_list = apply_filter(filtered_list, guess, feedback)
             
-        if guess == wordle_word or feedback == 'ggggg':
+        if (guess == wordle_word) or (feedback == 'ggggg'):
             answered_correct = True
             print(f'You got the wordle! The word is {guess}')
             print('It took you ' + str(num_guess) + ' guesses')
@@ -122,14 +139,34 @@ def play(ai=False, output=False):
         
         print(f'You guessed {guess}')
         print(f'The feedback is: {feedback}')
-        
-        if output:
-            print(filtered_list)
-            print(len(filtered_list))
+        print(f'Possible Answers:\n{filtered_list}')
+    
+    if num_guess >= MAX_GUESSES:
+        print('You lost....')
+    
+
+def play_gui(ai):
+    app = ctk.CTk()
+    app.title("Wordle Helper")
+    app.geometry("500x500")
+    
+    app.mainloop()
+
+def play(ai=False, ui=False):
+    if not ui:
+        play_in_terminal(ai)
+    else:
+        play_gui(ai)
         
         
 guessable_words_list, answers_list = get_words_lists(path=PATH)
-wordle_word = 'moral'
+wordle_word = random.choice(answers_list)
+answered_correct = False
+initial_guess = True
+filtered_list = answers_list.copy()
+num_guess = 0
 
-
-play(ai=True, output=True)
+try:
+    play(ui=False)
+except KeyboardInterrupt:
+    sys.exit()
